eb0955b6a66ff1111dbaba6cff6246a6064e99de
Andrey <alestro@bk.ru>
2020-06-28 23:47:33 +0200

    Update and rename src/contracts/PipelineFactory.php to src/PipelineFactoryInterface.php

---
+++ src/PipelineFactoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Bermuda\Pipeline;
+
+
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+
+/**
+ * Interface PipelineFactoryInterface
+ * @package Bermuda\Pipeline
+ */
+interface PipelineFactoryInterface
+{
+    /**
+     * @param MiddlewareInterface[] $middleware
+     * @param RequestHandlerInterface|null $handler
+     * @return PipelineInterface
+     */
+    public function __invoke(array $middleware = [], RequestHandlerInterface $handler = null): PipelineInterface ;
+    
+    /**
+     * @param MiddlewareInterface[] $middleware
+     * @param RequestHandlerInterface|null $handler
+     * @return PipelineInterface
+     */
+    public function make(array $middleware = [], RequestHandlerInterface $handler = null): PipelineInterface ;
+    
+}
