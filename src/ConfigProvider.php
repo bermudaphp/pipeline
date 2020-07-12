5a631fe22b7cf0ab74fdbd6b8f08624b9136a9db
Andrey <alestro@bk.ru>
2020-07-12 23:37:30 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -8,6 +8,10 @@ final class ConfigProvider
 {
     public function __invoke(): array
     {
-        return ['dependencies' => ['factories' => [PipelineInterface::class => PipelineFactory::class}]]];
+        return ['dependencies' => [
+                    'factories' => [PipelineInterface::class => PipelineFactoryInterface::class}], 
+                    'autowires' => [PipelineFactoryInterface::class => PipelineFactory::class]
+               ]
+        ];
     }
 }
