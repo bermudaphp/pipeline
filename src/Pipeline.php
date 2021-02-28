93c1cdaf7360ae1d436204959d74dbdf5a2984e6
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-02-28 03:43:45 +0300

    Update Pipeline.php

--- src/Pipeline.php
+++ src/Pipeline.php
@@ -2,12 +2,10 @@
 
 namespace Bermuda\Pipeline;
 
-
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-
 
 /**
  * Class Pipeline
