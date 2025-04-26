4654f4640297e30bfe9a8cf65f4fc846d8ee3330
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-26 03:14:04 +0200

    Update PipelineFactory.php

--- src/PipelineFactory.php
+++ src/PipelineFactory.php
@@ -1,39 +1,16 @@
 <?php
 
-namespace Bermuda\Pipeline;
+namespace Bermuda\Http\Middleware;
 
 use Psr\Container\ContainerInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-/**
- * Class PipelineFactory
- * @package Bermuda\Pipeline
- */
-class PipelineFactory implements PipelineFactoryInterface
+
+final class PipelineFactory implements PipelineFactoryInterface
 {
-    public const containerMiddlewareId = '\Bermuda\Pipeline\PipelineFactory@middlewareId';
-    public const containerFallbackHandlerId = '\Bermuda\Pipeline\PipelineFactory@fallbackHandlerId';
-
-    /**
-     * @param ContainerInterface $container
-     * @return Pipeline
-     */
-    public function __invoke(ContainerInterface $container): PipelineInterface
+    public function createMiddlewarePipeline(iterable $middlewares = [], ?RequestHandlerInterface $fallbackHandler = null): PipelineInterface
     {
-        return $this->make(
-            $container->has(self::containerMiddlewareId) ?
-                $container->get(self::containerMiddlewareId) : null,
-            $container->has(self::containerFallbackHandlerId) ?
-                $container->get(self::containerFallbackHandlerId) : null
-        );
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function make(?iterable $middleware = [], ?RequestHandlerInterface $fallbackHandler = null): PipelineInterface
-    {
-        return !empty($middleware) ? Pipeline::makeOf($middleware, $fallbackHandler) : new Pipeline($fallbackHandler);
+        return new Pipeline($middlewares, $fallbackHandler ?? new EmptyPipelineHandler);
     }
 }
