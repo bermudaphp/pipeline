c62f45dbe996c8f573044183b02985c78840c4bf
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-26 03:15:46 +0200

    Update PipelineInterface.php

--- src/PipelineInterface.php
+++ src/PipelineInterface.php
@@ -1,19 +1,26 @@
 <?php
 
-namespace Bermuda\Pipeline;
+namespace Bermuda\Http\Middleware;
 
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-/**
- * Interface PipelineInterface
- * @package Bermuda\Pipeline
- */
 interface PipelineInterface extends MiddlewareInterface, RequestHandlerInterface
 {
     /**
-     * @param MiddlewareInterface $middleware
-     * @return PipelineInterface
+     * Adds middleware to the pipeline.
+     *
+     * Accepts either a single MiddlewareInterface instance or an iterable collection of middleware.
+     * A cloned pipeline instance is created and the provided middleware are appended,
+     * ensuring that the original pipeline remains immutable.
+     *
+     * @param iterable<MiddlewareInterface>|MiddlewareInterface $middlewares A middleware or collection of middleware objects.
+     *
+     * @return PipelineInterface Returns a new pipeline instance with the added middleware.
+     *
+     * @throws \InvalidArgumentException If any provided element does not implement MiddlewareInterface.
+     *         Error Message: "Provided middleware (index) does not implement MiddlewareInterface"
+     * @throws \RuntimeException If the middleware being added is the pipeline itself.
      */
-    public function pipe(MiddlewareInterface $middleware): PipelineInterface ;
+    public function pipe(iterable|MiddlewareInterface $middlewares): PipelineInterface ;
 }
