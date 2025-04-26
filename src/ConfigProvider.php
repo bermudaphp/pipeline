cbd64b042e3f76180395cc3f95a875c4fe4785a9
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-26 03:11:24 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,26 +1,28 @@
 <?php
 
-namespace Bermuda\Pipeline;
+namespace Bermuda\Http\Middleware;
 
 use Psr\Container\ContainerInterface;
+use function Bermuda\Config\conf;
 
-/**
- * Class ConfigProvider
- * @package Bermuda\Pipeline
- */
 final class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
+    public const string CONFIG_KEY_MIDDLEWARES = 'Bermuda\Http\Middleware\Pipeline:middlewares';
+    public const string CONFIG_KEY_FALLBACK_HANDLER = 'Bermuda\Http\Middleware\Pipeline:fallbackHandler';
+
     /**
      * @inheritDoc
      */
     protected function getFactories(): array
     {
-        return [PipelineInterface::class => PipelineFactoryInterface::class];
+        return [PipelineInterface::class => static function (ContainerInterface $container): PipelineInterface {
+            return $container->get(PipelineFactoryInterface::class)->createMiddlewarePipeline(
+                ($conf = conf($container))->get(self::CONFIG_KEY_MIDDLEWARES, []),
+                $conf->get(self::CONFIG_KEY_FALLBACK_HANDLER)
+            );
+        }];
     }
-    
-    /**
-     * @inheritDoc
-     */
+
     protected function getInvokables(): array
     {
         return [PipelineFactoryInterface::class => PipelineFactory::class];
